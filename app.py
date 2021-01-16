f93e6f98a768f9a873cd782e8aef4eeaa997735c
Sagar Mahajan <66167736+sagarmahajan20@users.noreply.github.com>
2021-01-16 07:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,37 @@ def predict():
 
     return 'Car Detected at 5 metre'
 
+@app.route('/url/', methods=['GET']) #<string:url>
+def get_task():
+	url = request.args.get('url') 
+    person_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
+    #url = "https://maps.mapmyindia.com/place/P0015261904.jpg"
+
+    # METHOD #1: OpenCV, NumPy, and urllib
+    def url_to_image(url):
+        # download the image, convert it to a NumPy array, and then read
+        # it into OpenCV format
+        resp = urllib.request.urlopen(url)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        # return the image
+        return image
+    def DetectPerson(img):
+    # Read first frame
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Pass frame to our car classifier
+    persons = person_classifier.detectMultiScale(gray, scaleFactor=1.106, minNeighbors=2,minSize=(32, 32))
+        
+    # Extract bounding boxes for any bodies identified
+    for (x,y,w,h) in persons:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 2)
+    Txt="Person is detected"
+    img = url_to_image(url)  
+    DetectionText = DetectPerson(img)  
+
+	return {'object': "object found at 5 meters", "url" : label}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
